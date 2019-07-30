28513c195af6c116d46407a24598ebfe77e160a1
SeongChan Lee <sclee@codechain.io>
2019-07-30 21:50:11 +0900

    hello: Best block number 가져오는 기능 추가
    SDK를 초기화해 hello 커맨드에 넘겨줍니다. getBestBlockNumber()함수는
    async 함수이므로 await해야하고, 그러려면 hello 역시 async함수여야
    합니다. 그러면 우리 index.ts 역시도 async여야 하는데 쉽지 않습니다.
    index.ts 전체 코드를 async 함수 안에 집어넣고, 마지지막에 async함수를
    직접 호출합니다.
    썩 맘에 들지는 않습니다만 nodejs가 pending promise가 끝나기 전까지
    종료되지 않는 동작특성을 이용해서 전체 코드를 async하게 실행할 수
    있습니다. async-await 은 이렇게 함수를 두 부류(async/blocking)으로
    구분지어서 async함수는 async함수 안에서만 호출할 수 있게 구분짓습니다.

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,9 +1,14 @@
+import { SDK } from "codechain-sdk";
+
 import { Tracer } from "../tracer";
 
-export default function hello(tracer: Tracer, args: string[]) {
+export default async function hello(sdk: SDK, tracer: Tracer, args: string[]) {
   console.log(`Hello, ${tracer.state.nickname}!`);
   if (args.length > 0) {
     console.log(`Your name has been changed to ${args[0]}`);
     tracer.state.nickname = args[0];
   }
+
+  const bestBlockNumber = await sdk.rpc.chain.getBestBlockNumber();
+  console.log("Best block number:", bestBlockNumber);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import * as process from "process";
 
+import { SDK } from "codechain-sdk";
+
 import hello from "./commands/hello";
 import { Tracer } from "./tracer";
 
-// console.log(process.argv);
-const [command, ...args] = process.argv.slice(2);
+async function asyncMain() {
+  // console.log(process.argv);
+  const [command, ...args] = process.argv.slice(2);
 
-const tracer = Tracer.load();
-switch (command) {
-  case "hello":
-    hello(tracer, args);
-    break;
-  default:
-    throw new Error(`Invalid command ${command}`);
+  const tracer = Tracer.load();
+
+  const sdk = new SDK({
+    server: "https://corgi-rpc.codechain.io",
+    networkId: "wc",
+  });
+
+  switch (command) {
+    case "hello":
+      await hello(sdk, tracer, args);
+      break;
+    default:
+      throw new Error(`Invalid command ${command}`);
+  }
+  tracer.save();
 }
-tracer.save();
+
+asyncMain().catch(e => {
+  console.error(e);
+});
