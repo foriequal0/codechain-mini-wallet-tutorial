32b45ba4e99489bfcd3ce2f636808309bc56090b
SeongChan Lee <sclee@codechain.io>
2019-07-30 21:00:28 +0900

    hello: 첫번째 서브커맨드
    앞으로 새 기능들은 이런식으로 추가할 예정입니다.
    ts-node나 node등의 런타임으로 스크립트를 실행하면 argv[0]에는 실행하는
    런타임의 경로가 argv[1]에 실행하는 스크립트의 경로가, argv[2]부터 입력한
    인자들이 뒤따라옵니다. 입력한 인자 첫번째를 받아서 hello 함수를
    실행하게끔 합니다.

---
+++ src/commands/hello.ts
@@ -0,0 +1,4 @@
+export default function hello(args: string[]) {
+  console.log("Hello, World!");
+  console.log(args);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import * as process from "process";
+
+import hello from "./commands/hello";
+
+// console.log(process.argv);
+const [command, ...args] = process.argv.slice(2);
+
+switch (command) {
+  case "hello":
+    hello(args);
+    break;
+  default:
+    throw new Error(`Invalid command ${command}`);
+}
